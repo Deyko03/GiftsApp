1728cccaca2f147ca5fe75006e953dc083a9fb39
Deyko03 <deykerlanzavargas395@gmail.com>
2026-08-13 15:57:47 -0600

    pensemos en componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+    import('./gifts/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: 'trending',
+    loadComponent: () =>
+    import('./gifts/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+    import('./gifts/pages/dashboard-page/dashboard-page.component'),
+  },
+
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+
+];
---
+++ src/app/gifts/pages/dasboard-page/dasboard-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dasboard-page',
+  imports: [],
+  templateUrl: './dasboard-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DasboardPage {}
