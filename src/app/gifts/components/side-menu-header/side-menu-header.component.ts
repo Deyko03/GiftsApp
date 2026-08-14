a6a4427d4bf2e1dfecfb51d01490559ab9862830
Deyko03 <deykerlanzavargas395@gmail.com>
2026-08-14 10:29:21 -0600

    Angular Environments y Path Alias

--- src/app/gifts/components/side-menu-header/side-menu-header.component.ts
+++ src/app/gifts/components/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifts-side-menu-header',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './side-menu-header.html',
 
 })
-export class SideMenuHeader {}
+export class SideMenuHeader {
+
+  envs = environment;
+
+}
