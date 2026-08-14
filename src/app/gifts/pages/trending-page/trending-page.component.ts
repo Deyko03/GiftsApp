07e602358ebbcb13550f572874c549129f02d41a
Deyko03 <deykerlanzavargas395@gmail.com>
2026-08-14 11:19:21 -0600

    Mostrar imágenes de relleno

--- src/app/gifts/pages/trending-page/trending-page.component.ts
+++ src/app/gifts/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GiftList } from "../../components/gif-list/gif-list";
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GiftList],
   templateUrl: './trending-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
