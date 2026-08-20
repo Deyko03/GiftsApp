ab8f5917f93d19021dde457c3176c74cff42a398
Deyko03 <deykerlanzavargas395@gmail.com>
2026-08-20 15:43:38 -0600

    Diseño de pantalla - Buscador de Gifs

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GiftList } from "../../components/gif-list/gif-list";
+import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GiftList],
   templateUrl: './search-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPage {}
+export default class SearchPage {
+
+  gifService = inject( GifService )
+
+  onSearch(query: string) {
+  this.gifService.searchGifs(query);
+
+  }
+
+}
